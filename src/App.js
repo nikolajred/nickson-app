0f508cd5cb80c01c4c1f66271e25455d1b9fed46
Nikolay Red <40564813+nikolajred@users.noreply.github.com>
2021-10-21 00:01:23 +0300

    began social network

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Skills from './Skills';
+// import logo from './eagle.png'
+
 
 
 const App = () => {
   return (
-  <div>
-     <Header/>
-     <Skills/>
+  <div className="app-wrapper">
+     <header className="header">
+       <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/travels-logo-png-for-business-design-template-03d1ab18244e3126af5a7b41554e7c99_screen.jpg?ts=1566599731" alt="eagle"></img> 
+     </header>
+     <nav className="nav">
+       <div>Profile</div>
+       <div>Messages</div>
+       <div>News</div>
+       <div>Musik</div>
+     </nav>
+     <div className="content">Main content</div>
   </div>
   );
 }
