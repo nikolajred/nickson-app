6f94796166c7ce9604d85b818f80718d6f001de6
Nikolay Red <40564813+nikolajred@users.noreply.github.com>
2021-11-07 03:52:09 +0200

    added links

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import Navbar from './components/Navbar/Navbar';
 import Content from './components/Content/Content';
 import Dialogs from './components/Dialogs/Dialogs';
 
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 const App = () => {
     return (
+
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-           <div className='app-wrapper-content'>
-             <Dialogs/>
-             {/*<Content/>*/}
-           </div>
-
+            <div className='app-wrapper-content'>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='content' element={<Content/>}/>
+                        <Route path='dialogs' element={<Dialogs/>}/>
+                        <Route path='news' element={<News/>}/>
+                        <Route path='music' element={<Music/>}/>
+                        <Route path='settings' element={<Settings/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
 
         </div>
+
+
     )
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {Link, Outlet} from "react-router-dom";
 
-const Navbar = () =>{
-    return(
+
+const Navbar = () => {
+    return (
+
         <nav className={classes.nav}>
-        <div className={classes.item}><a href="#s">Profile</a></div>
-        <div className={classes.item}><a href="#s">Messages</a></div>
-        <div className={classes.item}><a href="#s">News</a></div>
-        <div className={classes.item}><a href="#s">Musik</a></div>
-        <div className={classes.item}><a href="#s">Settings</a></div>
-    </nav>);
-}
+            <View className={classes.item}>
+                <Link to='/content'>Profile</Link>
+            </View>
+            <View className={classes.item}>
+                <Link to='/dialogs'>Messages</Link>
+            </View>
+            <View className={classes.item}>
+                <Link to="/news">News</Link>
+            </View>
+            <View className={classes.item}>
+                <Link to="/music">Music</Link>
+            </View>
+            <View className={classes.item}>
+                <Link to="/settings">Settings</Link>
+            </View>
+
+            <Outlet />
+        </nav>
+
+    )
+};
 export default Navbar;
