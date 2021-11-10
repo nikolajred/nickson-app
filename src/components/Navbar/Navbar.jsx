939098ee9360c9a22a2ac451f9a3cac470f4164a
Nikolay <nikolajred@gmail.com>
2021-11-10 23:36:59 +0200

    added activeClassNames

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,35 @@ import {Link} from "react-router-dom";
 
 
 const Navbar = () => {
+
+    
     return (
         <nav className={classes.nav}>
             <nav>
-                <Link className={classes.item} to="/content">Profile</Link>
-                <Link className={classes.item} to="/dialogs">Dialogs</Link>
-                <Link className={classes.item} to="/news">News</Link>
-                <Link className={classes.item} to="/music">Music</Link>
-                <Link className={classes.item} to="/settings">Settings</Link>
+                <div className={classes.item}>
+                    <Link   to="/content" activeClassName={classes.active} >
+                        Profile
+                    </Link>
+                </div>
+
+                <div className={classes.item}>
+                    <Link   to="/dialogs" activeClassName={classes.active}>
+                        Dialogs
+                    </Link>
+                </div>
+
+                <div className={classes.item}>
+                    <Link  to="/news">News</Link>
+                </div>
+
+                <div className={classes.item}>
+                    <Link  to="/music">Music</Link>
+                </div>
+
+                <div className={classes.item}>
+                    <Link  to="/settings">Settings</Link>
+                </div>
+
             </nav>
         </nav>
 
