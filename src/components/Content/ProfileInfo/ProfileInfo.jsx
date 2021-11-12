01b34f1d3b79b47fcb4577b01d46d2f92a34f604
Nikolay <nikolajred@gmail.com>
2021-11-12 23:17:54 +0200

    added props in dialogs

---
+++ src/components/Content/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ocean from "../../../img/ocean.jpg";
+import avatar from "../../../img/avatar.jpg";
+import classes from './ProfileInfo.module.css';
+
+
+const ProfileInfo = () =>{
+    return(
+        <div>
+            <img src={ocean} className={classes.content_ocean} alt="ocean" />
+            <div>
+                <img src={avatar} className={classes.content_avatar} alt="avatar"/>
+                <div className={classes.description}>
+                    <h2>Василий Петренко</h2>
+                    <h3>Дата рождения: 01 августа 1983г.</h3>
+                    <h3>г. Харьков</h3>
+                    <h3>Украина</h3>
+                    <h3>Харьковский полетехнический институт</h3>
+                </div>
+            </div>
+        </div>);
+}
+export default ProfileInfo;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return(
+        <div className={classes.item}>
+            <NavLink to={'/dialogs/'+ props.id} >
+                {props.name}
+            </NavLink>
+        </div>
+
+    )
+};
+
+const MessageItem = (props) => {
+    return(
+        <div className={classes.item}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return(
         <div className={classes.dialogs}>
             <div className={classes.dialog}>
-                <div className={classes.item +' '+ classes.activeItem}>Nikolay</div>
-                <div className={classes.item}>Ivan</div>
-                <div className={classes.item}>Sveta</div>
-                <div className={classes.item}>Maxim</div>
-                <div className={classes.item}>Masha</div>
+                <DialogItem id='1' name='Nikolas'/>
+                <DialogItem id='2' name='Ivan'/>
+                <DialogItem id='3' name='Sandra'/>
+                <DialogItem id='4' name='Maxim'/>
+                <DialogItem id='5' name='Masha'/>
+                <DialogItem id='6' name='Valera'/>
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi!</div>
-                <div className={classes.message}>How are you?</div>
-                <div className={classes.message}>Glad to see you!</div>
+                <MessageItem message = 'Hi!'/>
+                <MessageItem message = 'How are you?'/>
+                <MessageItem message = 'Glad to see you!'/>
             </div>
         </div>
 
