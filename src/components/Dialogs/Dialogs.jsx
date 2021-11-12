7348375f74f556c16bfd7c3932a17cffa8ff961a
Nikolay <nikolajred@gmail.com>
2021-11-12 23:51:51 +0200

    added arreys in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,16 +19,31 @@ const MessageItem = (props) => {
     )
 }
 
+const DialogsData = [
+    {id: '1', name: 'Nikolas'},
+    {id: '2', name: 'Ivan'},
+    {id: '3', name: 'Sandra'},
+    {id: '4', name: 'Maxim'},
+    {id: '5', name: 'Masha'},
+    {id: '6', name: 'Valera'},
+];
+
+const MessagesData = [
+    {id: '1', message: 'Hi!'},
+    {id: '2', message: 'How are you?'},
+    {id: '3', message: 'Glad to see you!'}
+]
+
 const Dialogs = (props) => {
     return(
         <div className={classes.dialogs}>
             <div className={classes.dialog}>
-                <DialogItem id='1' name='Nikolas'/>
-                <DialogItem id='2' name='Ivan'/>
-                <DialogItem id='3' name='Sandra'/>
-                <DialogItem id='4' name='Maxim'/>
-                <DialogItem id='5' name='Masha'/>
-                <DialogItem id='6' name='Valera'/>
+                <DialogItem id = {DialogsData[0].id} name={DialogsData[0].name}/>
+                <DialogItem id = {DialogsData[1].id} name={DialogsData[1].name}/>
+                <DialogItem id = {DialogsData[2].id} name={DialogsData[2].name}/>
+                <DialogItem id = {DialogsData[3].id} name={DialogsData[3].name}/>
+                <DialogItem id = {DialogsData[4].id} name={DialogsData[4].name}/>
+                <DialogItem id = {DialogsData[5].id} name={DialogsData[5].name}/>
             </div>
             <div className={classes.messages}>
                 <MessageItem message = 'Hi!'/>
